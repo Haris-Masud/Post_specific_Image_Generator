a80f4c56d387ac119f6466dbd94f17364d96dbe6
Muhammad Haris Masud <haristiwana7272@gmail.com>
2025-04-27 19:03:34 +0500

    updated

--- image_generation.py
+++ image_generation.py
@@ -17,9 +17,9 @@ st.set_page_config(page_title="Brand Based Social Media Image Generator", layout
 
 # ─── Load environment ─────────────────────────────────────────────────────────
 load_dotenv()
-MONGO_URI = st.secrets["MONGODB_URI"]
-GENAI_API_KEY = st.secrets["GENAI_API_KEY"]
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+MONGO_URI      = os.getenv("MONGODB_URI")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GENAI_API_KEY  = os.getenv("GENAI_API_KEY")
 
 
 # Directory paths (near where you create OUT_DIR)
